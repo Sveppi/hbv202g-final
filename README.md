689a30f4fd5de2566009afdf1d38e01f3f6b5cb7
Sverrir Sigfusson <s.sigfusson@icloud.com>
2024-04-05 10:38:17 +0000

    readme updated

---
+++ README.md
@@ -0,0 +1,40 @@
+# Project Name
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+## Description
+
+'Málsháttur' is a randomizer that allows you to get a random idiom from a collection of over 800 Icelandic idioms, perfect for Easter! You can also favorite up to ten idioms at a time.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contact](#contact)
+
+## Installation
+
+'mvn install' compiles the program.
+
+## Usage
+
+Run the program with the command 'mvn exec:java'
+
+You interact with the program through a CLI. At each step the valid options are printed for you. Simply type in what you want to do.
+
+- "nyjan" : get a random idiom
+- "vista" : save the current idiom to favorites
+- "skoda" : view your favorites
+- "haetta" : quit the program
+
+While in the favorites view you can also delete from the favorites by typing the number of the favorite. You can return to the main randomizer by typing "baka".
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Contact
+
+- Email: svs37@hi.is
+- GitHub: [Sveppi](https://github.com/Sveppi)
--- src/main/java/is/hi/hbv202g/finalass/ChoiceHandlers.java
+++ src/main/java/is/hi/hbv202g/finalass/ChoiceHandlers.java
@@ -22,6 +22,16 @@ public class ChoiceHandlers {
         this.scanner = scanner;
     }
 
+    /**
+     * Handles the user's choice by calling the appropriate method based on the choice.
+     * The choice is obtained from the user using the getChoice() method.
+     * If the choice is "nyjan", the handleNyjanChoice() method is called.
+     * If the choice is "vista", the handleVistaChoice() method is called.
+     * If the choice is "skoda", the handleSkodaChoice() method is called.
+     * If the choice is "haetta", the handleHaettaChoice() method is called.
+     * If the choice is invalid, the printInvalidChoice() method is called and the user is prompted to enter a valid choice.
+     * This process continues until a valid choice is entered.
+     */
     public void handleChoice() {
         String choice = getChoice();
         switch (choice) {
@@ -44,16 +54,34 @@ public class ChoiceHandlers {
         }
     }
 
+    /**
+     * Prompts the user with the main instruction and returns the user's choice as a string.
+     *
+     * @return the user's choice as a string
+     */
     private String getChoice() {
         Print.printMainInstr();
         return scanner.nextLine();
     }
 
+    /**
+     * Handles the "Nyjan" choice by printing the available idioms and returning the current idiom.
+     *
+     * @return The current idiom.
+     */
     private int handleNyjanChoice() {
         Print.printIdiom(idioms);
         return idioms.getCurrent();
     }
 
+    /**
+     * Handles the "Vista" choice.
+     * Adds the current idiom to the favorites list and updates the current idiom.
+     * If the idiom is successfully added to the favorites list, it prints a message indicating the addition.
+     * If the idiom is already in the favorites list, it prints the idiom without adding it again.
+     * 
+     * @return The index of the current idiom.
+     */
     private int handleVistaChoice() {
         boolean saved = favs.addFavorite(Idioms.getIdiomCollection()[current]);
         if (saved) {
@@ -65,6 +93,11 @@ public class ChoiceHandlers {
         return current;
     }
 
+    /**
+     * Handles the Skoda choice by allowing the user to interact with their favorite choices.
+     * The user is prompted to select a favorite choice and can choose to remove it from their favorites or return to the previous menu.
+     * If an invalid choice is entered, the user is prompted to enter a valid choice.
+     */
     private void handleSkodaChoice() {
         boolean inFav = true;
 
@@ -87,6 +120,11 @@ public class ChoiceHandlers {
         }
     }
 
+    /**
+     * Handles the "Haetta" choice.
+     * 
+     * This method prints a quit message and exits the program.
+     */
     private void handleHaettaChoice() {
         Print.printQuit();
         System.exit(0);
